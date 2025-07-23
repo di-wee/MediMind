5cd4629a67a86d8b91b5f80157249832652f675f
Dionis Wee <callmehdion@gmail.com>
2025-07-23 11:48:39 +0800

    started frontend component hierarchy and routing

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+//wrapper component that will check if login is still valid before showing page
+function ProtectedRoute({ children }) {
+	// temp logic, to replace with UseEffect once backend Auth API is developed
+	const isLoggedIn = localStorage.getItem('isLoggedIn') === true;
+
+	if (!isLoggedIn) {
+		return (
+			<Navigate
+				to='/login'
+				replace
+			/>
+		);
+	}
+
+	return children;
+}
+
+export default ProtectedRoute;
