066bf67972853ae386bf3fe950c060e931ee10ab
Dionis Wee <callmehdion@gmail.com>
2025-07-28 14:17:54 +0800

    basic UI for filter container

--- frontend/src/components/FilterContainer.jsx
+++ frontend/src/components/FilterContainer.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 function FilterContainer({ filterOptions }) {
 	return (
 		<>
-			<div className='filter-container'>
-				<h3 className='text-xs'>Filter by:</h3>
+			<div className='filter-container text-center'>
+				<div className='px-2'>
+					<h3 className='text-xs'>Filter by:</h3>
+					{filterOptions.map((op) => (
+						<div class='flex items-center mt-2 mb-2'>
+							<input
+								id='default-checkbox'
+								type='checkbox'
+								className='input-checkbox'
+							/>
+							<label
+								for='default-checkbox'
+								className='ms-2 text-xs text-gray-600'>
+								{op}
+							</label>
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	);
