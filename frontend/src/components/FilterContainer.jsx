f6944ac32280f7d4000a934656d2fce567a3862e
Dionis Wee <callmehdion@gmail.com>
2025-07-28 13:33:01 +0800

    created filter container

---
+++ frontend/src/components/FilterContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FilterContainer({ filterOptions }) {
+	return (
+		<>
+			<div className='filter-container'>
+				<h3 className='text-xs'>Filter by:</h3>
+			</div>
+		</>
+	);
+}
+
+export default FilterContainer;
--- frontend/src/components/MedicationList.jsx
+++ frontend/src/components/MedicationList.jsx
@@ -2,11 +2,25 @@ import { CheckIcon, FunnelIcon } from '@heroicons/react/20/solid';
 import React, { useEffect, useState } from 'react';
 import MedicationLog from './MedicationLog';
 import medsList from '../mockdata/medicationlist.json';
+import FilterContainer from './FilterContainer';
 
 function MedicationList({ patientId }) {
 	const [medicationList, setMedicationList] = useState([]);
 	const [selectedMedicine, setSelectedMedicine] = useState({});
 	const [visible, setVisible] = useState(false);
+	const [filterKey, setFilterKey] = useState(null);
+
+	const uniqueStatus = [
+		...new Set(
+			medicationList.map((med) => (med.isActive ? 'Active' : 'Inactive'))
+		),
+	];
+
+	const uniqueMissedDoseStatus = [
+		...new Set(
+			medicationList.map((med) => (med.missedDose ? 'Missed' : 'Compliant'))
+		),
+	];
 
 	const handleMedicationClick = (meds) => {
 		//if user clicks on medication again, it will set visibility to false
@@ -36,11 +50,43 @@ function MedicationList({ patientId }) {
 							<th>Medication Name</th>
 							<th>Dosage</th>
 							<th>Frequency</th>
+
 							<th>
-								Status <FunnelIcon className='filter-btn' />
+								<div className='relative inline-flex items-center gap-1'>
+									Status
+									<FunnelIcon
+										onClick={() =>
+											setFilterKey((prev) =>
+												prev === 'status' ? null : 'status'
+											)
+										}
+										className='filter-btn'
+									/>
+								</div>
+								{filterKey === 'status' ? (
+									<FilterContainer filterOptions={uniqueStatus} />
+								) : (
+									''
+								)}
 							</th>
-							<th>
-								Missed Dose <FunnelIcon className='filter-btn' />
+
+							<th className='relative'>
+								<div className='relative inline-flex items-center gap-1'>
+									Missed Dose
+									<FunnelIcon
+										onClick={() =>
+											setFilterKey((prev) =>
+												prev === 'missedDose' ? null : 'missedDose'
+											)
+										}
+										className='filter-btn'
+									/>
+								</div>
+								{filterKey === 'missedDose' ? (
+									<FilterContainer filterOptions={uniqueMissedDoseStatus} />
+								) : (
+									''
+								)}
 							</th>
 						</tr>
 					</thead>
