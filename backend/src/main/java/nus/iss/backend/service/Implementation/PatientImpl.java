7181676c624da2d6d1bb47e285e608d1fb14d186
Dionis Wee <callmehdion@gmail.com>
2025-08-02 13:36:33 +0800

    Patient repo, impl and service created

---
+++ backend/src/main/java/nus/iss/backend/service/Implementation/PatientImpl.java
@@ -0,0 +1,10 @@
+package nus.iss.backend.service.Implementation;
+
+import nus.iss.backend.service.PatientService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PatientImpl implements PatientService {
+}
