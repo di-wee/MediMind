76d98aac167ba598af157717d653eee24db032e5
Dionis Wee <callmehdion@gmail.com>
2025-08-05 13:59:35 +0800

    commit

--- backend/src/main/java/nus/iss/backend/service/Implementation/MedicationImpl.java
+++ backend/src/main/java/nus/iss/backend/service/Implementation/MedicationImpl.java
@@ -1,5 +1,9 @@
 package nus.iss.backend.service.Implementation;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nus.iss.backend.dao.ImageOutput;
+import nus.iss.backend.dto.EditMedicationRequest;
 import nus.iss.backend.exceptions.ItemNotFound;
 import nus.iss.backend.model.Medication;
 import nus.iss.backend.model.Patient;
@@ -12,6 +16,10 @@ import nus.iss.backend.util.ImageToApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
@@ -20,6 +28,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.HttpHeaders;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -33,7 +45,16 @@ public class MedicationImpl implements MedicationService {
     MedicationRepository medicationRepo;
 
     @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
     ScheduleService scheduleService;
+
+    @Autowired
+    PatientService patientService;
+
+    private final DateTimeFormatter formatterNoColon = DateTimeFormatter.ofPattern("HHmm");
+    private final DateTimeFormatter formatterWithColon = DateTimeFormatter.ofPattern("HH:mm");
 
     @Override
     public Boolean hasMedicineMissedDose(UUID medicationId) {
@@ -70,5 +91,83 @@ public class MedicationImpl implements MedicationService {
     @Override
     public Medication saveMedication(Medication medication) {
         return medicationRepo.save(medication);
+    }
+
+    @Override
+    public ImageOutput sendToFastAPI(MultipartFile file) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        InputStreamResource fileResource = new ImageToApi(
+                file.getInputStream(), file.getOriginalFilename());
+        body.add("file", fileResource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        String fastapiUrl = "http://192.168.0.115:8000/predict_image"; // Replace with your local FastAPI IP
+
+        ResponseEntity<String> response = restTemplate.postForEntity(fastapiUrl, requestEntity, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode extracted = root.get("extracted_info");
+
+        ImageOutput result = new ImageOutput();
+        result.setMedicationName(extracted.path("medication_name").asText(""));
+        result.setIntakeQuantity(extracted.path("intake_quantity").asText(""));
+        result.setFrequency(Integer.parseInt(extracted.path("frequency").asText("")));
+        result.setInstructions(extracted.path("instruction").asText(""));
+        result.setNotes(extracted.path("note").asText(""));
+
+        return result;
+    }
+
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> processEditMedication(EditMedicationRequest req) {
+        Medication med = this.findMedicineById(req.getMedicationId());
+        if (med == null) {
+            return ResponseEntity.status(404).body("Medication not found");
+        }
+
+        Optional<Patient> patientOpt = patientService.findPatientById(req.getPatientId());
+        if (patientOpt.isEmpty()) {
+            return ResponseEntity.status(404).body("Patient not found");
+        }
+        Patient patient = patientOpt.get();
+
+        //every time will clean all inactive schedules(created more than 90 days) and related intakeHistory
+        LocalDateTime cutoffDate = LocalDateTime.now().minusDays(90);
+        scheduleService.deleteOldInactiveSchedules(med, cutoffDate);
+
+        //then deactivate the active schedules before create new ones
+        List<Schedule> activeSchedules = scheduleService.findActiveSchedulesByMedication(med);
+        scheduleService.deactivateSchedules(activeSchedules);
+
+        //then update new frequency
+        med.setFrequency(req.getFrequency());
+        this.saveMedication(med);
+
+        //then create new schedules
+        for (String timeStr : req.getTimes()) {
+            LocalTime time;
+            try {
+                if (timeStr.contains(":")){
+                    time = LocalTime.parse(timeStr,formatterWithColon);
+                } else {
+                    time = LocalTime.parse(timeStr, formatterNoColon);
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Invalid time format: " + timeStr);
+            }
+
+            scheduleService.createSchedule(med, patient, time);
+        }
+
+        //TODO:last step: reset the alarm and notification things,
+        // leave to shiying to implement, probably will implement in android part, not sure
+
+        return ResponseEntity.ok("Medication details updated successfully");
     }
 }
