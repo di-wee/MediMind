d479eb9ba9895ee88eda23fe7e9d4216d081ad81
Dionis Wee <callmehdion@gmail.com>
2025-08-05 13:53:33 +0800

    Reapply "Merge remote-tracking branch 'origin/backend-(java-springboot)' into backend--Pris"
    This reverts commit 739e51e4ed69cbcddbc17cdf3cebda39a0810f22.

--- backend/src/main/java/nus/iss/backend/service/MedicationService.java
+++ backend/src/main/java/nus/iss/backend/service/MedicationService.java
@@ -1,7 +1,9 @@
 package nus.iss.backend.service;
 
 import nus.iss.backend.dao.ImageOutput;
+import nus.iss.backend.dto.EditMedicationRequest;
 import nus.iss.backend.model.Medication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,4 +26,5 @@ public interface MedicationService {
 
     // For OCR + NER integration
     ImageOutput sendToFastAPI(MultipartFile file) throws IOException;
+    ResponseEntity<?> processEditMedication(EditMedicationRequest req);
 }
