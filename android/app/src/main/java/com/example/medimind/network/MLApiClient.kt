54b0a41b961f5f48f0319bca930750516807d22c
Prizellia <priscillaleongbl@gmail.com>
2025-08-16 13:03:12 +0800

    tidied up IP address

--- android/app/src/main/java/com/example/medimind/network/MLApiClient.kt
+++ android/app/src/main/java/com/example/medimind/network/MLApiClient.kt
@@ -7,21 +7,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object MLApiClient {
     // FastAPI endpoints for ML service
-<<<<<<< Updated upstream
-    private const val EMULATOR_URL = "http://47.130.114.135:8000"
-    private const val DEVICE_URL = "http://192.168.1.3:8000" // Update this to your local IP address
-    
-=======
-    private const val EMULATOR_URL = "http://10.0.2.2:8000" //run locally
-    //private const val EMULATOR_URL = "http://10.0.2.2:8001/" //run ec2 model
-    //private const val DEVICE_URL = "http://172.20.10.3:8000" //for physical android phone (when model is run LOCALLY on pris' com)
-    private const val DEVICE_URL = "http://10.0.2.2:8000" //for emulator when connecting to model running locally on pris com
-    //private const val DEVICE_URL = "http://47.130.114.135:8001/" //connecting to model hosted on ec2 instance
-    //10.0.2.2:8001
+    // For running locally
+    private const val EMULATOR_URL = "http://10.0.2.2:8000"
+    private const val DEVICE_URL = "http://192.168.1.3:8000"  // if using physical android device change to laptop IP on WiFi
 
->>>>>>> Stashed changes
-    // For production/EC2 (commented out)
-    // private const val PRODUCTION_URL = "http://54.255.65.62:8000"
+    // Change to this for running on EC2. Instance
+    //private const val EMULATOR_URL = "http://47.130.114.135:8000" // Works for both emulator and physical device
 
     private fun isEmulator(): Boolean {
         val fingerprint = android.os.Build.FINGERPRINT
